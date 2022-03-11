684bc9e1e1aa64120a082387295cde1a89aa2cc0
Emily Liang <eliang@lcs.on.ca>
2022-03-11 17:43:27 -0500

    Initial Commit

---
+++ Currency Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency Converter
+//
+//  Created by Xiaoyu Liang on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
