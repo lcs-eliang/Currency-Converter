7d36f1551cf3d899e75b5b3241ce8aac962421e3
Emily Liang <eliang@lcs.on.ca>
2022-03-17 16:23:06 -0400

    successfully got the currency datas from the currency api by using dummy datas.

--- Currency Converter/ContentView.swift
+++ Currency Converter/ContentView.swift
@@ -8,10 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rate:String = "Loading"
+    @State private var currencyType:String = "LSL"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("USD:"+currencyType+"= 100:"+rate)
+            .padding().onAppear {
+                apiRequest(url: "https://api.exchangerate.host/latest?base=USD&amount=100") {currencyData in
+                    for (key, value) in currencyData.rates {
+                        if key == currencyType {
+                            rate = String(value)
+                            break
+                        }
+                        print("\(key) -> \(value)")
+                    }
+                    //print(currencyData)
+                    
+                    }
+            }
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Currency Converter/CurrencyData.swift
@@ -0,0 +1,37 @@
+//
+//  CurrencyData.swift
+//  Currency Converter
+//
+//  Created by Xiaoyu Liang on 2022-03-13.
+//
+
+import Foundation
+
+struct CurrencyData: Codable {
+    var success: Bool
+    var base: String
+    var date: String
+    var rates = [String: Double]()
+}
+
+func apiRequest(url: String, completion: @escaping (CurrencyData) -> ()) {
+    let urlO = URL(string:url)!
+    var request = URLRequest(url: urlO)
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.httpMethod = "GET"
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        
+        guard let data = data, error == nil else {
+            print("An error occurs")
+            return
+        }
+        if let curData = try? JSONDecoder().decode(CurrencyData.self, from: data) {
+            completion(curData)
+        } else {
+            
+            print("Invalid response from server.")
+        }
+        
+    }.resume()
+}
